a8a7de806ca715ceb816ab1556f9dad85ebba07b
Muggi <masterdagon@hotmail.com>
2015-05-05 21:58:26 +0200

    customer + cityinfo entities

---
+++ src/java/entity/Customer.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Dennnis
+ */
+@Entity
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String fName,lName,street,country;
+   
+    @OneToMany
+    private List<CityInfo> cityList;  
+
+    public Customer() {
+    }
+
+    public Customer(String fName, String lName) {
+        this.fName = fName;
+        this.lName = lName;
+        this.cityList = new ArrayList();
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<CityInfo> getCityList() {
+        return cityList;
+    }
+
+    public void setCityList(List<CityInfo> cityList) {
+        this.cityList = cityList;
+    }
+    
+    
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
