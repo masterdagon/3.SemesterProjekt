ca058b0965deaf92ad986bb107e381f379653cce
Dennis <Dennisheegaard@gmail.com>
2015-05-06 22:49:06 +0200

    Facade Rest Flight FlightInstance

--- src/java/Rest/ApplicationConfig.java
+++ src/java/Rest/ApplicationConfig.java
@@ -29,7 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().		
      */		
     private void addRestResourceClasses(Set<Class<?>> resources) {		
-        resources.add(Rest.flights.class);		
+        resources.add(Rest.flights.class);
     }		
     		
 }		
