d20863f05a07e1895ad865cd9d48a7802e08edb5
Muggi <masterdagon@hotmail.com>
2015-05-06 15:42:36 +0200

    facade + flightinstance + populatetables

--- src/java/entity/FlightInstance.java
+++ src/java/entity/FlightInstance.java
@@ -46,13 +46,13 @@ public class FlightInstance implements Serializable {
     public FlightInstance() {
     }
 
-    public FlightInstance(String airline, Date date, double price, boolean bookingCode,Airport arrival,Airport departure,Plane plane) {
+    public FlightInstance(String airline, Date date, double price,Airport arrival,Airport departure,Plane plane) {
         this.airline = airline;
         this.date = date;
         this.price = price;
         this.freeSeats = plane.getTotalSeats();
         this.reservations = new ArrayList();
-        this.bookingCode = bookingCode;
+        this.bookingCode = true;
         this.arrival = arrival;
         this.depature = departure;
         this.plane = plane;
--- src/java/test/PopulateTables.java
+++ src/java/test/PopulateTables.java
@@ -5,7 +5,9 @@
  */
 package test;
 
+import static entity.FlightInstance_.price;
 import facade.Facade;
+import java.util.Date;
 
 /**
  *
@@ -18,8 +20,11 @@ public class PopulateTables {
     public static void main(String[] args) {
         f.createCityInfo("Kokkedal", 2980);
         f.createAirport("CPH", "Copenhagen","Denmark");
+        f.createAirport("FBB", "Berlin", "Germany");
         f.createPlane("Airbus A350", 253);
         f.createCustomer("Martin", "Rasmussen", "Egedalsvænge", "Denmark", 2980);
+        Date date = new Date();
+        f.createFlightInstance("SAS", date, 800, true, null, null, null)
     }
     
 }
